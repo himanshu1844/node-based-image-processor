50a885a80c9830744aa15aa4cc37b34142ee9db3
Himanshu Pancholi <2022uch1043@mnit.ac.in>
2025-04-15 07:18:40 +0000

    Changes

--- build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/nodeeditor/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Himanshu Pancholi/ImageProcessor/nodeeditor
-# Build directory: C:/Users/Himanshu Pancholi/ImageProcessor/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/nodeeditor
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external")
--- build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/nodeeditor/external/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Himanshu Pancholi/ImageProcessor/nodeeditor/external
-# Build directory: C:/Users/Himanshu Pancholi/ImageProcessor/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/nodeeditor/external
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
